2ed261c7258f812bead3152c53c12335184f7503
Luke Shore <Luke.A.shore@outlook.com>
2024-05-23 23:41:58 +0100

    clean up

--- Kernel/src/main.zig
+++ Kernel/src/main.zig
@@ -1,8 +1,21 @@
 const console = @import("console.zig");
 const mm = @import("memory/MemoryManager.zig");
 const MultiBoot = @import("MultiBoot.zig");
-export fn kernel_main(multiboot_magic: *const u32, multiboot_info_ptr: *const MultiBoot.struct_multiboot_info) callconv(.C) void {
-    _ = multiboot_magic;
+
+const flags: u32 = MultiBoot.MULTIBOOT_INFO_MEMORY | MultiBoot.MULTIBOOT_PAGE_ALIGN;
+
+export const header: MultiBoot.multiboot_header linksection(".multiboot") = .{
+    .magic = MultiBoot.MULTIBOOT_HEADER_MAGIC,
+    .flags = flags,
+    .checksum = MultiBoot.MULTIBOOT_HEADER_MAGIC & flags,
+};
+
+export fn _start() callconv(.Naked) noreturn {
+    kernel_main(&header);
+    while (true) {}
+}
+
+fn kernel_main(multiboot_info_ptr: *const MultiBoot.multiboot_info) void {
     console.initialize();
     //print multiboot info to console
     console.printf("multiboot info: {}\n", .{multiboot_info_ptr});
